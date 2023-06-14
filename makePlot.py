69b835775d64c944736bb95b62e52727b7fe0675
Jovan Mitrevski <jmitrevs@fnal.gov>
2023-06-14 10:57:54 -0500

    Add utilities

---
+++ makePlot.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import ROOT
+
+infile = ROOT.TFile("output.root", "read")
+
+print("The file contains:")
+keys = infile.GetListOfKeys()
+for key in keys:
+    print(key.GetName())
+
+graph0 = infile.Get("graph_0")
+c1 = ROOT.TCanvas("c1","c1",1600,1000)
+graph0.Draw()
+c1.SaveAs("graph0.png")
+
+hist0 = infile.Get("hist_0")
+c2 = ROOT.TCanvas("c2","c2",1600,1000)
+hist0.Draw("SURF2")
+c2.SaveAs("hist0.png")
---
+++ setup.sh
@@ -0,0 +1,2 @@
+source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
+setup dunesw v09_48_01d00 -q e20:prof
